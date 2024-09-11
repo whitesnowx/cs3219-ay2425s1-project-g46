800ddb6e6faa0168e2b5e0e710c7e1914122590a
unknown <xuelinglow09@gmail.com>
2024-09-12 01:47:08 +0800

    Testing form

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 5000 
+
+app.use(cors()); 
+app.use(express.json()); 
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Welcome to the Express server!');
+});
+app.post('/submit', (req, res) => {
+  const { inputText } = req.body; 
+  console.log('Received input:', inputText);
+  res.status(200).json({ message: `Form data received successfully: ${inputText}` });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
