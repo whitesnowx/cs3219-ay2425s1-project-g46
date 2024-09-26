df7f20ff4c4b942ab89d531d28705ff1d4715767
AndrewOng2066 <e0959899@u.nus.edu>
2024-09-27 02:39:09 +0800

    Move Signup.js and SignupValidation.js to user-service directory

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Signup from './Signup'
+import Signup from './user-service/Signup'
 import Login from "./user-service/Login"
 
 function App() {
