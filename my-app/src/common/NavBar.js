6819c7b3fb8b6f60d8e1daf6e7c3fcb89388eb3f
iynixil <lixinyilxy02@gmail.com>
2024-09-26 21:15:40 +0800

    Add navbar component and its styling

---
+++ my-app/src/common/NavBar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav id="navBar">
+      <div id="navBarLogo">
+        <a href="/">PeerPrep</a>
+      </div>
+      <div id="navBarContent">
+        <ul id="navBarLinks">
+          <li>
+            <a href="/signup">Sign Up</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
