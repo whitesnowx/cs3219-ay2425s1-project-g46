9e155315fee35889a80bd9a46ce18adaaaf7b357
tsulim <limxiujia18@gmail.com>
2024-09-29 04:48:09 +0800

    Add css to question page

--- my-app/src/pages/QuestionPage.js
+++ my-app/src/pages/QuestionPage.js
@@ -1,36 +1,50 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./QuestionPage.css";
 import axios from "axios";
 
 function QuestionPage() {
-    const { questionId } = useParams();
-    const [questionData, setQuestionData] = useState([]);
+  const { questionId } = useParams();
+  const [questionData, setQuestionData] = useState([]);
 
-    useEffect(() => {
-        const fetchQuestion = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5000/question/${questionId}`);
-                setQuestionData(response.data);
-            } catch (error) {
-                console.error("Error fetching question:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/question/${questionId}`);
+        setQuestionData(response.data);
+      } catch (error) {
+        console.error("Error fetching question:", error);
+      }
+    };
 
-        fetchQuestion();
-    }, [questionId]);
+    fetchQuestion();
+  }, [questionId]);
 
-    if (!questionData) {
-        return <div>Loading...</div>
-    }
+  if (!questionData) {
+    return <div>Loading...</div>
+  }
 
-    return (
-        <div>
-            <h1>{questionData.title}</h1>
-            <p>Category: {questionData.category}</p>
-            <p>Complexity: {questionData.complexity}</p>
-            <p>Description: {questionData.description}</p>
+  return (
+    <div id="questionContainer" class="container">
+      {/* Title of Question */}
+      <div class="row">
+        <h1 id="questionTitle">{questionData.title}</h1>
+      </div>
+
+      <div id="questionTagContainer" class="row">
+        <div class="questionTag">
+          {questionData.category}
         </div>
-    )
+        <div class="questionTag">
+          {questionData.complexity}
+        </div>
+      </div>
+
+      <div class="row">
+        <p>{questionData.description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default QuestionPage;
