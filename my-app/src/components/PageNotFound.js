04e8fc0546066dab3f0f776b55f46070637d4ef6
tsulim <limxiujia18@gmail.com>
2024-09-29 17:37:24 +0800

    Add component for Error404

---
+++ my-app/src/components/PageNotFound.js
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div id="errorContainer" class="container">
+      <h1>Error 404: Page Not Found</h1>
+      <div class="row">
+        <button id="backBtn" class="btn" onClick={() => navigate(-1)}>Back</button>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
