e917eb235efeeb5649f93275bbd678ec229deaf9
tsulim <limxiujia18@gmail.com>
2024-10-20 18:26:33 +0800

    Add components for collaboration page

---
+++ frontend/src/components/QuestionPanel.js
@@ -0,0 +1,26 @@
+// Author(s): Xiu Jia
+const QuestionPanel = ({questionData}) => {
+
+  return (
+    <div id="questionContainer" className="container">
+      <div className="row">
+        <h1 id="questionTitle">{questionData.title}</h1>
+      </div>
+
+      <div id="questionTagContainer" className="row">
+        <div className="questionTag">
+          {questionData.category}
+        </div>
+        <div className="questionTag">
+          {questionData.complexity}
+        </div>
+      </div>
+
+      <div className="row">
+        <p>{questionData.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionPanel;
