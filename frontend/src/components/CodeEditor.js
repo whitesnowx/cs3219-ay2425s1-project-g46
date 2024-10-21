9291136f7d86675dc93f56a5c28d98c3074c080a
unknown <xuelinglow09@gmail.com>
2024-10-22 02:46:22 +0800

    Implement tabs to change between code editor and content editor

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,18 +1,18 @@
-// Author(s): Xiu Jia
+// Author(s): Xue ling
 import "./styles/CodeEditor.css";
 import { useEffect, useRef, useState } from "react";
 import socket from "../pages/collaboration-service/utils/socket";
-import Editor from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
 
 const CodeEditor = ({ roomId }) => {
-  const [code, setCode] = useState("", "")
+  const [code, setCode] = useState("", "");
   const editorRef = useRef(null);
 
   useEffect(() => {
     console.log(roomId);
     socket.on("receiveCode", ({ code }) => {
       setCode(code);
-    })
+    });
 
     return () => {
       socket.off("receiveCode");
@@ -20,29 +20,28 @@ const CodeEditor = ({ roomId }) => {
   }, [code]);
 
   function handleEditorChange(code, event) {
-    console.log(code)
     setCode(code);
     socket.emit("sendCode", { roomId, code });
   }
 
   return (
     <div id="editor-container" className="container">
-      <Editor 
-      height="100vh"
-      language={"javascript"}
-      defaultValue={"// your code here"}
-      value={code}
-      theme="vs-dark" 
-      onChange= {handleEditorChange}
-      options={{
-        minimap: {
-          enabled: true, // Enable minimap
-          renderCharacters: true, // Show characters in the minimap
-        },
-      }}
+      <Editor
+        height="100vh"
+        language={"javascript"}
+        defaultValue={"// your code here"}
+        value={code}
+        theme="vs-dark"
+        onChange={handleEditorChange}
+        options={{
+          minimap: {
+            enabled: true,
+            renderCharacters: true, // Show characters in the minimap
+          },
+        }}
       />
     </div>
-  )
-}
+  );
+};
 
 export default CodeEditor;
--- frontend/src/pages/collaboration-service/CollaborationPage.js
+++ frontend/src/pages/collaboration-service/CollaborationPage.js
@@ -1,25 +1,51 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import ContentEditor from "../../components/ContentEditor";
 import CodeEditor from "../../components/CodeEditor";
 
-import NavBar from "../../components/NavBar"
-import QuestionPanel from "../../components/QuestionPanel"
+import NavBar from "../../components/NavBar";
+import QuestionPanel from "../../components/QuestionPanel";
 
 const CollaborationPage = () => {
   const location = useLocation();
   const data = location.state.data;
   const { roomId, questionData } = data;
+  const [activeTab, setActiveTab] = useState("code", "");
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
 
   return (
     <div>
       <NavBar />
       <QuestionPanel questionData={questionData} />
-      <form>
-        <CodeEditor roomId={roomId} />
-        <ContentEditor roomId={roomId} />
-      </form>
+
+      <div className="tabs">
+        <button
+          onClick={() => handleTabChange("code")}
+          className={activeTab == "code" ? "active" : ""}
+        >
+          Code
+        </button>
+        <button
+          onClick={() => handleTabChange("content")}
+          className={activeTab == "content" ? "active" : ""}
+        >
+          Text
+        </button>
+      </div>
+      <div className="tab-content">
+        {/* Render both components with inline styles for visibility control */}
+        <div style={{ display: activeTab == "code" ? "block" : "none" }}>
+          <CodeEditor roomId={roomId} />
+        </div>
+        <div style={{ display: activeTab == "content" ? "block" : "none" }}>
+          <ContentEditor roomId={roomId} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CollaborationPage;
